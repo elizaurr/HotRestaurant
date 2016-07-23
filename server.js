8b9703490f893bd5b808b018faec30d13a9fd851
Todd Germaine <trgermaine@gmail.com>
2016-07-23 11:43:33 -0400

    added routing data

--- server.js
+++ server.js
@@ -44,8 +44,6 @@ var table = [
   }
 ]
 
-
-
 // On the click the newTable data will be added
 
     $('#addBtn').on("click", function(){
@@ -68,6 +66,31 @@ var table = [
       return false;
 
     }); 
+
+    // Routing Examples
+
+    // Basic route examples that sends the user first to the AJAX Page
+
+    app.get('/', function(req, res){
+      res.sendFile(path.join(__dirname, 'index.html'));
+    })
+
+    app.get('/add', function(req, res){
+      res.sendFile(path.join(__dirname, 'tables.html'));
+    })
+
+    app.get('/all', function(req, res){
+      res.sendFile(path.join(__dirname, 'reservations.html'));
+    })
+
+     app.get('/add', function(req, res){
+      res.sendFile(path.join(__dirname, 'add.html'));
+    })
+
+    app.get('/all', function(req, res){
+      res.sendFile(path.join(__dirname, 'all.html'));
+    })
+
 
     // Code to push all data on the click
 
